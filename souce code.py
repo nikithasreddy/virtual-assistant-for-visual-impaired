4b7dc539b4159ee714eaba2b2ce469e211157e66
nikithasreddy <114330623+nikithasreddy@users.noreply.github.com>
2022-09-25 20:25:35 +0530

    Update souce code.py

--- souce code.py
+++ souce code.py
@@ -25,6 +25,25 @@ import matplotlib.pyplot as plt
 from pprint import pprint
 from utils import *
 from matplotlib import pyplot as plt
+from flask import Flask, render_template, request
+import pandas as pd
+from sklearn.externals import joblib
+from PIL import Image
+import numpy as np
+import urllib.request
+import urllib.parse
+import keras
+from keras.models import load_model
+from keras.preprocessing import image as im
+
+from skimage import color
+from skimage import io
+# img = color.rgb2gray(io.imread('image.png'))
+import pytesseract
+# ---------------------------
+
+import tensorflow as tf
+global graph,model2,autoencoder
 
 max_val = 8
 max_pt = -1
@@ -361,226 +380,169 @@ class VideoCamera(object):
         engine.runAndWait()
         self.video.release()
         
+# detection of currency notes
+graph = tf.get_default_graph()
 
-# orb is an alternative to SIFT
-#test_img = read_img('files/test_100_2.jpg')
-test_img = read_img('files/test_50_1.jpg')
-#test_img = read_img('files/download.png')
-#test_img = read_img('files/test_100_3.jpg')
-#test_img = read_img('files/test_20_4.jpg')
-display('original1', test_img)
-# resizing must be dynamic
-original = resize_img(test_img, 0.4)
-#display('original', original)
-# keypoints and descriptors
-# (kp1, des1) = orb.detectAndCompute(test_img, None)
-(kp1, des1) = orb.detectAndCompute(test_img, None)
-training_set = ['files/20.jpg', 'files/50.jpg', 'files/100.jpg',
-'files/500.jpg']
-for i in range(0, len(training_set)):
-# train image
-train_img = cv2.imread(training_set[i])
-(kp2, des2) = orb.detectAndCompute(train_img, None)
-# brute force matcher
-bf = cv2.BFMatcher()
-all_matches = bf.knnMatch(des1, des2, k=2)
-good = []
-# give an arbitrary number -> 0.789
-# if good -> append to list of good matches
-for (m, n) in all_matches:
-if m.distance < 0.789 * n.distance:
-good.append([m])
-if len(good) > max_val:
-max_val = len(good)
-max_pt = i
-max_kp = kp2
-print(i, ' ', training_set[i], ' ', len(good))
-52
+app = Flask(__name__)
+model2 = load_model('final.h5')    #Lenet-aw.h5
 
+autoencoder = load_model('happyHack.h5')
 
+# model = keras.Sequential()
+# model = load_model('LeNetArch.h5')
+# model = load_model('litemodel.sav')
+# model = joblib.load('lenet-aw.h5')   
+# 'NoteDetection.h5'  'litemodel.sav'  LeNetArch.h5
 
-# read image as is
-def read_img(file_name):
-img = cv2.imread(file_name)
-return img
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def get():
+	input = dict(request.form)
+	# accidentindex = input['accidentindex'][0]
+	# age_of_vehicle = input['age_of_vehicle'][0]
 
-# resize image with fixed aspect ratio
-def resize_img(image, scale):
-res = cv2.resize(image, None, fx=scale, fy=scale, interpolation =
-cv2.INTER_AREA)
-return res
+	# if request.method == 'POST':  
+	#     f = request.files['file']  
+	#     f.save(f.filename) 
 
+	# file = request.files['file']
+	# if request.method == 'POST':  
+	#     f = request.files['file']
+	#     f.save(f.filename)  
+	# filename= request.form['hidden']  #input['imgname']
+	# filename = request.form.get('imgname')
+	print("filename=",request.form.get('imgname'))  
 
-# convert image to grayscale
-def img_to_gray(image):
-img_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-return img_gray
+	print("filename=",input['imgname'][0])   
 
+	v= input['imgname'][0]
+	#session
+	# s = session['key']
+	# print("s =",s) 
+	 # --------------------------------------------------------------
+	 # grey scale
+	# img = color.rgb2gray(io.imread(v))
+	# img = im.load_img(v, target_size=(256, 256))
+	# img = color.rgb2gray(io.imread(img))
 
-# gaussian blurred grayscale
-def img_to_gaussian_gray(image):
-img_gray = cv2.GaussianBlur(img_to_gray(image), (5, 5), 0)
-return img_gray
+	# data = np.array([np.array(img)]) 
+	# data = np.array([np.array(Image.open(v))])  #f.filename
+	i=cv2.imread(v,0); # input as grey scale
+	width = 256
+	height = 256
+	dim = (width, height)
 
+	# resize image
+	resized = cv2.resize(i, dim, interpolation = cv2.INTER_AREA)
 
-# convert image to negative
-def img_to_neg(image):
-utils.py:
-if max_val != 8:
-print(training_set[max_pt])
-print('good matches ', max_val)
-train_img = cv2.imread(training_set[max_pt])
-img3 = cv2.drawMatchesKnn(test_img, kp1, train_img, max_kp, good, 4)
-note = str(training_set[max_pt])[6:-4]
-print('\nDetected denomination: Rs. ', note)
-engine = pyttsx3.init()
-engine.say(note)
-engine.runAndWait()
-(plt.imshow(img3), plt.show())
-else:
-print('No Matches')
-engine = pyttsx3.init()
-engine.say("this is fake note")
-engine.runAndWait()
-53
-img_neg = 255 - image
-return img_neg
+	data = np.array([np.array(resized)])
+	# -----------------------------------------------------
 
 
-# binarize (threshold)
-# retval not used currently
-def binary_thresh(image, threshold):
-retval, img_thresh = cv2.threshold(image, threshold, 255,
-cv2.THRESH_BINARY)
-return img_thresh
+	# data = np.array([np.array(Image.open('500.jpg'))])  #/utilities/
+	data = data.reshape(-1,256,256,1)
+	data = data.astype('float32')
+	data = data/ 255.
+	# print("logging ",data)
+	# return str(data);
 
+	# model2._make_predict_function()
 
-# NO IDEA HOW THIS WPRKS
-def adaptive_thresh(image):
-img_thresh = cv2.adaptiveThreshold(image, 255,
-cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 8)
-# cv2.adaptiveThreshold(src, maxValue, adaptiveMethod, thresholdType,
-blockSize, C[, dst]) → dsta
-return img_thresh
+	with graph.as_default():
+	 y = model2.predict(data, verbose=1)
 
+	# try: y =model2.predict(data, verbose=1)
+	# except Exception as e: y = str(e)
 
-# sobel edge operator
-def sobel_edge(image, align):
-img_horiz = cv2.Sobel(image, cv2.CV_8U, 0, 1)
-img_vert = cv2.Sobel(image, cv2.CV_8U, 1, 0)
-if align == 'h':
-return img_horiz
-elif align == 'v':
-return img_vert
-else:
-print('use h or v')
+	# y =model2.predict(data, verbose=1)
+	# y = y.astype('float32')
+	# r = [round(x[0]) for x in y]
+	# r = float(round(y[0][0]))
+	# res = np.array(y, dtype='float64')
+	# res = np.array(r, dtype='float64')
+	print(" Y =  ",y)
+	# print(" res =  ",res)
+	# result = int(round(y[0][0])) 
+	# https://thispointer.com/find-max-value-its-index-in-numpy-array-numpy-amax/
+	result = np.where(y[0] == np.amax(y[0]))
+	print("result[0]=",result[0])
+	print("result[0][0]=",result[0][0])
+	pre=result[0][0]
+	# ------------------------
+	# return str(result[0][0])
 
+	# -------------------serial number -----------
+	img2 = cv2.imread(v, 0)
+	# cropped=img2[115:130, 30:100]
+	if pre == 0:
+	    cropped=img2[115:130, 30:100]     # 10-sns (30,115) (100,130)
+	elif pre == 1:
+	    cropped=img2[250:300, 70:265]     # 20-sns (70,250) (265,300)
+	elif pre == 2:    
+	    cropped=img2[450:520, 130:430] #100-sns
+	elif pre == 3:    
+	    cropped = img2[150:200, 75:390] #500-sns    
+	# cropped = img2[180:250, 80:550] #realmoney (75,150)(390 ,200)
 
-# sobel edge x + y
-def sobel_edge2(image):
-# ksize = size of extended sobel kernel
-grad_x = cv2.Sobel(image, cv2.CV_16S, 1, 0, ksize=3, borderType =
-cv2.BORDER_DEFAULT)
-grad_y = cv2.Sobel(image, cv2.CV_16S, 0, 1, ksize=3, borderType =
-cv2.BORDER_DEFAULT)
-abs_grad_x = cv2.convertScaleAbs(grad_x)
-abs_grad_y = cv2.convertScaleAbs(grad_y)
-dst = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
-return dst
+	config = ("-l eng --oem 3 --psm 6")
+	number = cv2.fastNlMeansDenoising(cropped, 10, 7,21)
+	number[number > 170] = 255
+	number[number <= 170] = 0
 
+	# config = ("-l eng --oem 3 --psm 3")
+	# kernel1 = np.ones((3, 3), np.uint8)
+	# kernel2 = np.ones((5, 5), np.uint8)
+	# number = cv2.morphologyEx(number, cv2.MORPH_CLOSE, kernel1)
+	#number = cv2.morphologyEx(number, cv2.MORPH_OPEN, kernel2)
+	# number = cv2.GaussianBlur(number, (3, 3), 0)       # For Smoothing
 
-# canny edge operator
-def canny_edge(image, block_size, ksize):
-# block_size => Neighborhood size
-# ksize => Aperture parameter for the Sobel operator
-# 350, 350 => for smaller 500
-# 720, 350 => Devnagari 500, Reserve bank of India
-img = cv2.Canny(image, block_size, ksize)
-54
-# dilate to fill up the numbers
-#img = cv2.dilate(img, None)
-return img
+	text = pytesseract.image_to_string(number, config=config)
+	print("text=",text)
 
+	# text="7AH 433534"
+	# --------------------counterfeit------------------
+	# xtest = data
 
-# laplacian edge
-def laplacian_edge(image):
-# good for text
-img = cv2.Laplacian(image, cv2.CV_8U)
-return img
+	xtest=cv2.imread(v, 0)
+	xtest = cv2.resize(xtest,(256,256))
+	xtest=xtest.reshape(-1,256,256,1)
+	xtest = xtest.astype('float32')
+	xtest = xtest/ 255.
 
+	with graph.as_default():
+	 decimg = autoencoder.predict(data, verbose=1)
+	# decimg = autoencoder.predict(xtest)
+	# xtest = data
+	# decimg = autoencoder._make_predict_function(xtest)
 
-# detect countours
-def find_contours(image):
-(_, contours, _) = cv2.findContours(image, cv2.RETR_LIST,
-cv2.CHAIN_APPROX_SIMPLE)
-contours = sorted(contours, key = cv2.contourArea, reverse = True)[:5]
-return contours
+	mse1 = np.mean(np.power(xtest - decimg, 2),axis=1)
+	mse0 = np.mean(mse1, axis=1)
+	print(mse0[0])
+	res=mse0[0]
+	con=0
+	if res > 0.002 :
+		if res < 0.01:
+			con=1
 
+	if con == 0:
+		return "-0-----------";	
+	# ----------------------------------
+	#  (denomination - 0/1/2/3) + (counterfeit= 0/1) + (sno)
+	ret = str(result[0][0]) + str("1") + text;
+	print("ret= ",ret);
+	return ret;
+    
+    
 
-# median blur
-def median_blur(image):
-blurred_img = cv2.medianBlur(image, 3)
-return blurred_img
-# dialte image to close lines
-def dilate_img(image):
-img = cv2.dilate(image, np.ones((5,5), np.uint8))
-return img
+# @app.route('/success', methods = ['POST'])  
+# def success():  
+#     if request.method == 'POST':  
+#         f = request.files['file']  
+#         f.save(f.filename)  
+#         return render_template("index.html", name = f.filename)  
 
-
-# erode image
-def close(image):
-img = cv2.Canny(image, 75, 300)
-img = cv2.dilate(img, None)
-img = cv2.erode(img, None)
-return img
-
-def harris_edge(image):
-img_gray = np.float32(image)
-corners = cv2.goodFeaturesToTrack(img_gray, 4, 0.03, 200, None, None,
-2,useHarrisDetector=True, k=0.04)
-corners = np.int0(corners)
-for corner in corners:
-x, y = corner.ravel()
-cv2.circle(image, (x, y), 3, 255, -1)
-return image
-
-
-# calculate histogram
-def histogram(image):
-hist = cv2.calcHist([image], [0], None, [256], [0, 256])
-# cv2.calcHist(images, channels, mask, histSize, ranges[, hist[,
-55
-accumulate]])
-plt.plot(hist)
-plt.show()
-
-
-# fast fourier transform
-def fourier(image):
-f = np.fft.fft2(image)
-fshift = np.fft.fftshift(f)
-magnitude_spectrum = 20 * np.log(np.abs(fshift))
-plt.subplot(121), plt.imshow(image, cmap='gray')
-plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(magnitude_spectrum, cmap='gray')
-plt.title('FFT'), plt.xticks([]), plt.yticks([])
-plt.show()
-
-
-# calculate scale and fit into display
-def display(window_name, image):
-screen_res = 1440, 900 # MacBook Air
-scale_width = screen_res[0] / image.shape[1]
-scale_height = screen_res[1] / image.shape[0]
-scale = min(scale_width, scale_height)
-window_width = int(image.shape[1] * scale)
-window_height = int(image.shape[0] * scale)
-# reescale the resolution of the window
-cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-cv2.resizeWindow(window_name, window_width, window_height)
-# display image
-cv2.imshow(window_name, image)
-# wait for any key to quit the program
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=4000)
